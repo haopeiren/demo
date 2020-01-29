df7b2058ff1602343cb4a6ae7d993fe1820836bd
haopeiren <445255842@qq.com>
2020-01-29 18:11:11 +0800

    update .gitignore

---
+++ utils/src/main/java/hpr/utils/cache/CacheProviderService.java
@@ -0,0 +1,9 @@
+package hpr.utils;
+
+/**
+ * @author haopeiren
+ * @since 2020/1/28
+ */
+public class cache
+{
+}
---
+++ utils/src/main/java/hpr/utils/cache/impl/LocalCacheServiceImpl.java
@@ -0,0 +1,9 @@
+package hpr.utils.cache.impl;
+
+/**
+ * @author haopeiren
+ * @since 2020/1/28
+ */
+public class LocalCacheServiceImpl
+{
+}
